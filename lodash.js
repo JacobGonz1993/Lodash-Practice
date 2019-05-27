2b5328c70d9612ffb23b2eb984971d00c4a837f4
rmcbride2121 <rmcbride2121@gmail.com>
2019-05-27 15:35:05 -0500

    added tests for all functions

--- lodash.js
+++ lodash.js
@@ -1,26 +1,53 @@
+"use strict"
+const assert = require("assert")
+
+
 var _ = require ("lodash")
-//heres this dumb array
-let arr = [1, 2, 3, 4];
-console.log('here is the array');
-console.log(arr);
+//here's this dumb array
+let arr = [1, 2, 3, 4]
+console.log('here is the array')
+console.log(arr)
 
 //chunk
-console.log('here is chunk');
-console.log(_.chunk(arr, 2));
+console.log('here is chunk')
+let chunk = _.chunk(arr, 2)
+console.log(chunk)
 
-//heres reverse
-console.log('here is reverse');
-console.log(_.reverse(arr));
+//here's reverse
+console.log('here is reverse')
+let reverse = _.reverse(arr)
+console.log(reverse)
 
-//heres without
-console.log('heres without 2 and 4');
-console.log(_.without((arr), 2, 4));
+//here's without
+console.log('heres without 2 and 4')
+let without = _.without((arr), 2, 4)
+console.log(without)
 
-//heres shuffle
-console.log('heres shuffle');
-console.log(_.shuffle(arr));
+//here's shuffle
+console.log('heres shuffle')
+let shuffle = _.shuffle(arr)
+console.log(shuffle)
 
-//heres head
-console.log('heres head');
-console.log(_.head(arr));
+//here's head
+console.log('heres head')
+let head = _.head(arr)
+console.log(head)
 
+//tests
+describe('lodash', function() {
+    it('chunk', function() {
+        assert.deepEqual(chunk, [[1,2],[3,4]]);
+    })
+    it('reverse', function() {
+        assert.deepEqual(reverse, [4, 3, 2, 1])
+    })
+    it('without', function() {
+        assert.deepEqual(without, [3, 1])
+    })
+    it('shuffle', function() {
+        assert.deepEqual(!shuffle, !arr)
+    })
+    it('head', function() {
+        assert.deepEqual(head, 4)
+    })
+})
